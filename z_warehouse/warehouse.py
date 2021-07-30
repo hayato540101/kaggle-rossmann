ba3a80ae02daea361457dcb01825b3062d5119b1
hayato540101 <5saphone92@gmail.com>
2021-07-30 23:32:26 +0900

    今日の前処理を関数化するところまで

--- z_warehouse/warehouse.py
+++ z_warehouse/warehouse.py
@@ -3,7 +3,7 @@ import os
 os.listdir('../../')
 
 
-'''EDA'''
+'''EDA&&基本的な前処理'''
 # 売り上げが0なのにOpenが1(=Openしているのに売り上げが0)なのはさすがにおかしい、実務だと理由がありそうだが、訓練データに関しては「売上0の時は店が開いていない」と置換してもよいはず
 # モデルはOpenの時売り上げが0になるとは限らないと学習してしまう
 # train[train['Sales']==0].Open.value_counts()
@@ -21,6 +21,18 @@ os.listdir('../../')
 # 条件式がFalseのものを置換する点がちょっと紛らわしいので注意
 train['Open'] = train['Open'].where(train['Sales']!=0, 0)
 train[train['Open']==0].Sales.value_counts()
+
+
+# 休日フラグを立ててみよう
+# The day of the week with Monday=0, Sunday=6.
+# 5と6を1、それ以外を0としてみる
+tmp = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 1, 6: 1}
+train['non-working day'] = train['DayOfWeek'].map(tmp)
+# こういうの確認するときはクエリの方が便利
+# train.query("DayOfWeek==5 or DayOfWeek==6")['DayOfWeek'].value_counts()
+
+
+
 
 
 '''特徴量'''
@@ -71,8 +83,10 @@ import pdb; pdb.set_trace()
 
 - メモリ削減はいいけど、int8,int16あたりにしているとどっかで列操作するときに上限・下限を超えて数値がバグってしまうことが考慮しなければならない。
 
-# 2021年7月30日
+# 2021年7月30日以降
 - コミットを意味のある単位で分けて、後から見て理解できるように
 - 他人の理解を助ける目的でのコミットメッセージを書く
+
+- 最後は関数に落とし込んだほうが作業内容が分けやすく何かと便利
 
 
